ed0392120df01407a6786aa6092866f628487179
Evelyn Suzarte <evelynsuzarte@hotmail.com>
2024-01-22 15:31:10 -0300

    mudança estrutura dos arquivos

--- cliente.py
+++ cliente.py
@@ -1,10 +1,15 @@
 import socket
-from threading import Thread
+from threading import Thread, Semaphore
 import time
 
 
 HOST = "localhost"
 PORT = 8000
+msg = ''
+
+sem_thread1 = Semaphore(1)
+sem_thread2 = Semaphore(0)
+
 
 def main():
     cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
@@ -15,29 +20,66 @@ def main():
     except:
         return print("Não foi possível conectar ao servidor.")
     
-    thread1 = Thread(target=enviar_msgs, args= [cliente])
-    thread2 = Thread(target=receber_msgs, args= [cliente])
+    #thread1 = Thread(target=enviar_msgs, args= [cliente])
+    thread1 = Thread(target=receber_msgs, args= [cliente])
 
     thread1.start()
-    thread2.start()
 
-
+#receber msgs do servidor
 def receber_msgs(cliente):
     while True:
         try:
-            msg = cliente.recv(1024).decode('utf-8')
+            #sem_thread1.acquire()
+            msg = cliente.recv(2048).decode('utf-8')
+
             if msg == "-1":
-                input("**** Digite a opção desejada novamente a seguir ****\n")
-                #enviar_msgs(cliente)
+                opcao = input("**** Digite a opção desejada novamente a seguir ****\n")
+                cliente.send(opcao.encode('utf-8'))
+            elif msg.count(',') >=1:
+                tratar_vetor(msg)
+            elif msg.count(',') == 0 and not msg[:9] == "solicitar":
+                print(msg)    
+            elif msg == "solicitar download":
+                #sem_thread2.acquire()
+                nome_arquivo = input("Digite o nome do arquivo para fazer download: \n")    
+                cliente.send(nome_arquivo+' download'.encode('utf-8'))
+                #time.sleep(20)
+                #time.sleep(5)
+            elif msg == "solicitar upload":
+                nome_arquivo = input("Digite o nome do arquivo para fazer upload: \n")
+                cliente.send(nome_arquivo+' upload'.encode('utf-8'))
+            
+            elif msg == "solicitar delecao":
+                nome_arquivo = input("Digite o nome do arquivo para deletar: \n")
+                cliente.send(nome_arquivo+' delecao'.encode('utf-8'))
+
         except:
             print ("Não foi possível continuar conectado ao servidor")
             cliente.close()
-            break
+            
+            return
+        #time.sleep(8)
+        
+            #break
+        
 
+
+#menu
+def menu():
+    print("------------ MEUS ARQUIVOS -------------\n")  
+    print("1. Listagem de arquivos do servidor")
+    print("2. Listagem de arquivos do cliente")
+    print("3. Download de arquivos do servidor")
+    print("4. Deleção de arquivos no servidor")
+    print("5. Upload de arquivos para o servidor")
+
+
+#enviar msgs para o servidor
 def enviar_msgs(cliente):
     while True:
         try:
-            #time(1)
+            #sem_thread2.acquire()
+            time.sleep(2)
             print("------------ MEUS ARQUIVOS -------------\n")  
             print("1. Listagem de arquivos do servidor")
             print("2. Listagem de arquivos do cliente")
@@ -47,9 +89,20 @@ def enviar_msgs(cliente):
 
             msg = input("Digite a opção desejada:\n->")
             cliente.send(msg.encode('utf-8'))
+
+            #time.sleep(40)
+           
+
         except:
             return
         
 
+def tratar_vetor(msg):
+    lista = []
+    lista = msg.split(",")
+    for item in lista:
+        print (item)
 
+ 
+menu()
 main()
--- servidor.py
+++ servidor.py
@@ -1,7 +1,12 @@
 import socket
-from threading import Thread
+from threading import Thread, Semaphore
 import os
 import shutil
+from datetime import datetime
+import time
+
+sem_thread1 = Semaphore(1)
+sem_thread2 = Semaphore(0)
 
 
 HOST = "localhost"
@@ -18,55 +23,95 @@ def main():
         servidor.bind((HOST,PORT))
         servidor.listen()
         print ("Servidor inicializado.")
+        escrever_log('Servidor iniciado')
     except:
-        return print ("Não possível inicializar o servidor.")
+        escrever_log('Não possível inicializar o servidor.')
+        print ("Não possível inicializar o servidor.")
+        return
     
     #adiciona cliente na lista de clientes e cria uma thread para ele
     while True:
         cliente, addr = servidor.accept()
         clientes.append(cliente)
         print("Cliente conectado:",addr)
+        escrever_log('Cliente conectado '+str(addr[1]))
+
         thread = Thread(target=processar_msgs, args=[cliente])
         thread.start()
 
 #processa as msgs enviadas pelo cliente
 def processar_msgs(cliente):
     while True:
+        #retorno = []
         try:
             msg = cliente.recv(1024).decode('utf-8')
-            if msg == '1':
-                print("\n---------- Listagem de arquivos do servidor----------\n")
-                listar_arquivos("SERVIDOR")
-                
-            elif msg == '2':
-                print("\n---------- Listagem de arquivos do cliente----------\n")
-                listar_arquivos("CLIENTE")
-                
-            elif msg == '3':
-                print("\n---------- Download de arquivos do servidor----------\n")
-                listar_arquivos("SERVIDOR")
-                nome_arquivo = input("Digite o nome do arquivo para fazer download: \n")
+            #sem_thread1.release()
+
+            if msg[-8:] == "download":
+                nome_arquivo = msg.split()[0]
                 copiar_arquivo("SERVIDOR","CLIENTE",nome_arquivo)
                 print("Download realizado com êxito!")
-                print("\n---------- Arquivos cliente ----------")
-                listar_arquivos("CLIENTE")     
-                          
-            elif msg == '4':
-                print("\n---------- Deleção de arquivos no servidor ---------- \n")
-                listar_arquivos("SERVIDOR")
-                nome_arquivo = input("Digite o nome do arquivo para deletar: \n")
+                enviar_msgs(cliente,"---------- Arquivos cliente ----------")
+                arquivos = listar_arquivos("CLIENTE")     
+                enviar_msgs(cliente, arquivos)
+                escrever_log('Solicitação de download de arquivos do servidor')
+                #time.sleep(5)
+            
+            elif msg[-7:] == "delecao":
+                nome_arquivo = msg.split()[0]
                 deletar_arquivo(nome_arquivo)
-                print("\n---------- Arquivos servidor ----------")
-                listar_arquivos("SERVIDOR")
-                  
-            elif msg == '5':
-                print("\n----------Upload de arquivos para o servidor ----------\n")
-                listar_arquivos("CLIENTE")
-                nome_arquivo = input("Digite o nome do arquivo para fazer upload: \n")
+                enviar_msgs(cliente,"---------- Arquivos servidor ----------")
+                arquivos = listar_arquivos("SERVIDOR")
+                enviar_msgs(cliente, arquivos)
+                escrever_log('Solicitação de deleção de arquivos do servidor')
+                time.sleep(2)
+            
+            elif msg[-6:] == "upload":
+                nome_arquivo = msg.split()[0]
                 copiar_arquivo("CLIENTE","SERVIDOR",nome_arquivo)
                 print("Upload realizado com êxito!")
-                print("\n---------- Arquivos servidor ----------")
-                listar_arquivos("SERVIDOR") 
+                enviar_msgs(cliente,"---------- Arquivos servidor ----------")
+                arquivos = listar_arquivos("SERVIDOR") 
+                enviar_msgs(cliente, arquivos)
+                escrever_log('Solicitação de upload de arquivos para o servidor')
+                time.sleep(2)
+
+            elif msg == '1':
+                enviar_msgs(cliente,"---------- Listagem de arquivos do servidor----------")
+                arquivos = listar_arquivos("SERVIDOR")
+                enviar_msgs(cliente, arquivos)
+                escrever_log('Solicitação de listagem de arquivos do servidor ')
+                time.sleep(2)
+
+            elif msg == '2':
+                enviar_msgs(cliente,"---------- Listagem de arquivos do cliente----------")
+                arquivos = listar_arquivos("CLIENTE")
+                enviar_msgs(cliente,arquivos)
+                escrever_log('Solicitação de listagem de arquivos do cliente')
+                time.sleep(2)
+
+            elif msg == '3':
+                enviar_msgs(cliente,"---------- Download de arquivos do servidor----------")
+                arquivos = listar_arquivos("SERVIDOR")
+                enviar_msgs (cliente,arquivos)
+                enviar_msgs (cliente,"solicitar download")
+                time.sleep(2)
+                          
+            elif msg == '4':
+                enviar_msgs(cliente,"---------- Deleção de arquivos no servidor ----------")
+                arquivos = listar_arquivos("SERVIDOR")
+                enviar_msgs (cliente,arquivos)
+                enviar_msgs (cliente,"solicitar delecao")
+
+                #time.sleep(1)
+                  
+            elif msg == '5':
+                enviar_msgs(cliente,"----------Upload de arquivos para o servidor ----------")
+                arquivos = listar_arquivos("CLIENTE")
+                enviar_msgs (cliente,arquivos)
+                enviar_msgs (cliente,"solicitar upload")
+
+                #time.sleep(1)
                 
             else:
                 print ("Opção desejada não existente!!")
@@ -75,23 +120,34 @@ def processar_msgs(cliente):
 
         except:
             delete_cliente(cliente)
-            break
+            time.sleep(5)
+            #escrever_log('Cliente removido: '+cliente)
+            #break
+        #sem_thread1.release()
+       
 
+#deletar clientes que não estão mais conectados
 def delete_cliente(cliente):
     clientes.remove(cliente)
 
+
+#enviar msg para cliente
 def enviar_msgs(cliente, msg):
     try:
         cliente.send(msg.encode('utf-8'))
     except:
         delete_cliente(cliente)
 
-
+#funções de manipulação de arquivos --------------------------------
 def listar_arquivos(pasta):
+    lista = []
     caminho_pasta = os.path.join(caminho_projeto,pasta)
     arquivos_na_pasta = os.listdir(caminho_pasta)
     for arquivo in arquivos_na_pasta:
-        print(arquivo)
+        nome = os.path.basename(arquivo)
+        lista.append(nome)
+    msg = tratar_vetor(lista)
+    return msg
 
 def copiar_arquivo(origem, destino, nome_arquivo):
     caminho_origem = os.path.join(caminho_projeto, origem, nome_arquivo)
@@ -107,4 +163,16 @@ def deletar_arquivo(nome_arquivo):
     else:
         print("\nO arquivo não existe.")
 
+def escrever_log(mensagem):
+    data_public = datetime.now()
+    data = data_public.strftime("%d/%m/%Y %H:%M")
+    arquivo = open ("logs.txt",'a')
+    arquivo.write(data + " : " + mensagem+"\n")
+    arquivo.close()
+
+def tratar_vetor(vetor):
+    tratado = ",".join(vetor)
+    return tratado 
+#-------------------------------------------------------------------
+        
 main()
